c6895a55d5738ac9a621b4ca603fe70ad0af0753
UGroisman <102042688+UGroisman@users.noreply.github.com>
2025-09-14 04:46:24 -0300

    fix player board&movement

--- src/player.c
+++ src/player.c
@@ -7,13 +7,15 @@
 
 
 typedef struct { 
-    int * board;
+    int board[8];
     unsigned short player_x, player_y;
 } movement_info;
 
 gameState * state_ptr;
 synchronization * sync_ptr;
-movement_info * movement_info_ptr;
+
+static movement_info mip;                 
+movement_info *movement_info_ptr = &mip;
 
 bool is_valid_move(int width, int height, unsigned short player_x, unsigned short player_y, int diff_x, int diff_y);
 int movement(int width, int height, int player_id);
@@ -26,9 +28,9 @@ int movement(int width, int height, int player_id){
     unsigned short player_x = movement_info_ptr->player_x, player_y=movement_info_ptr->player_y;
     for(int i=0; i < NMOVS; i++){ 
         if(is_valid_move(width,height,player_x, player_y , movs[i][0], movs[i][1])){
-            next_pos = (player_y + movs[i][1]) * width + player_x + movs[i][0];
-            if(movement_info_ptr->board[next_pos] > max){
-                max = movement_info_ptr->board[next_pos];
+           // next_pos = (player_y + movs[i][1]) * width + player_x + movs[i][0];
+            if(movement_info_ptr->board[i] > max){
+                max = movement_info_ptr->board[i];
                 m = i;
             }
         }
@@ -67,6 +69,7 @@ int get_id (){
 
 }
 
+/*
 int inic_local_game_info(int width, int height){
     movement_info_ptr = malloc(sizeof(movement_info));
     if (movement_info_ptr == NULL) { 
@@ -82,6 +85,7 @@ int inic_local_game_info(int width, int height){
     }
     return 0;
 }
+*/
 
 void free_local_game_info(){
     free(movement_info_ptr->board);
@@ -89,6 +93,7 @@ void free_local_game_info(){
     return;
 }
 
+/*
 void copy_state_info(int width, int height, int player_id){
     movement_info_ptr->player_x = state_ptr->players[player_id].pos_x;
     movement_info_ptr->player_y = state_ptr->players[player_id].pos_y;
@@ -116,6 +121,46 @@ void copy_state_info(int width, int height, int player_id){
         }
     }
 
+    return;
+    
+}
+*/
+
+void copy_state_info(int width, int height, int player_id){
+    movement_info_ptr->player_x = state_ptr->players[player_id].pos_x;
+    movement_info_ptr->player_y = state_ptr->players[player_id].pos_y;
+
+    //int row_to_copy, pos_aux;
+
+    for(int i=0; i < NMOVS; i++){
+        if(is_valid_move(width,height,movement_info_ptr->player_x, movement_info_ptr->player_y , movs[i][0], movs[i][1])){
+            movement_info_ptr->board[i] = state_ptr->board[(movement_info_ptr->player_y + movs[i][1] ) * width + movement_info_ptr->player_x + movs[i][0]];
+        }else{
+            movement_info_ptr->board[i] = 0 ;
+        }
+    }
+/*
+    //Copiar la fila en la que está el jugador y, de ser posible, la de arriba y la de abajo
+    if ((row_to_copy = movement_info_ptr->player_y-1) >= 0){
+        for (int i = 0; i < width; i++){
+            pos_aux = row_to_copy*width + i;
+            movement_info_ptr->board[pos_aux] = state_ptr->board[pos_aux];
+        }
+    }
+
+    row_to_copy=movement_info_ptr->player_y;
+    for (int i = 0; i < width; i++){
+        pos_aux = row_to_copy*width + i;
+        movement_info_ptr->board[pos_aux] = state_ptr->board[pos_aux];
+    }    
+
+    if ((row_to_copy = movement_info_ptr->player_y+1) < height){
+        for (int i = 0; i < width; i++){
+            pos_aux = row_to_copy*width + i;
+            movement_info_ptr->board[pos_aux] = state_ptr->board[pos_aux];
+        }
+    }
+*/
     return;
     
 }
@@ -149,13 +194,13 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-
+/*
     if (inic_local_game_info(width, height)<0){
         close_shm_sync(sync_ptr);
         close_shm_state(state_ptr);
         return 1;
     }
-    
+    */
     
     while (1) { 
         player_wait_turn(sync_ptr, my_id); 
@@ -181,7 +226,7 @@ int main(int argc, char *argv[]) {
     }
 
 
-    free_local_game_info();
+    //free_local_game_info();
     close_shm_sync(sync_ptr);
     close_shm_state(state_ptr);
     return 0;
